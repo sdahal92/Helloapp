657f7af11d5ebb52c211fe384e21cb02925156f7
Spandan Dahal <spandandahal101@gmail.com>
2024-12-07 13:24:00 -0700

    Update steamlitapp.py

--- steamlitapp.py
+++ steamlitapp.py
@@ -1,6 +1,1 @@
-├── app.py                # Main file (entry point)
-├── pages/                # Directory for additional pages
-│   ├── 1_Overview.py     # Page 1: Overview
-│   ├── 2_About_Us.py     # Page 2: About Us
-│   ├── 3_EDA.py          # Page 3: Exploratory Data Analysis
-│   ├── 4_Machine_Learning.py # Page 4: Machine Learning
+
